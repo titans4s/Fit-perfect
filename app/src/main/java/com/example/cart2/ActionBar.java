0ce4867fe8d3fca574c3db43028a5188d37cc601
hp <nivethikagnanaseelan@gmail.com>
2020-08-15 17:17:40 +0530

    commit

---
+++ app/src/main/java/com/example/cart2/ActionBar.java
@@ -0,0 +1,4 @@
+package com.example.cart2;
+
+public class ActionBar {
+}
---
+++ app/src/main/java/com/example/cart2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.cart2;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.ic_baseline_clear_24);
+
+
+    }
+}
